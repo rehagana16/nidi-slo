0c2126f7ec99752bd347411bc4b73327b5707a51
Rehagana Sembiring <rehagana.sembiring@swiperxapp.com>
2025-02-20 16:49:01 +0700

    finish navbar template

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,8 +2,15 @@ import styles from './navbar.module.css';
 
 export const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className={styles.container}>
+      <div>
+        Logo
+      </div>
+      <div className={styles.menuList}>
+        <div className={styles.menuButton}>Beranda</div>
+        <div className={styles.menuButton}>Buat Permohonan</div>
+        <div className={styles.menuButton}>Tarif Nidi & Slo</div>
+      </div>
     </div>
   )
 }
