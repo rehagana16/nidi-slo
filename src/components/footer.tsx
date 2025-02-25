681d1fe9b2dd20b32d52c5360e91177d68d7fa1f
Rehagana Sembiring <rehagana.sembiring@swiperxapp.com>
2025-02-25 23:26:29 +0700

    modify footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,26 +1,27 @@
 import styles from './footer.module.css';
+import baseStyles from './base-section-style.module.css';
+import mergeStrings from '@/util/mergeClassname';
 
 export const Footer = () => {
   return(
     <div className={styles.container}>
-      <div className={styles.column}>
-        <div className={styles.header}>
-          NAMA PERUSAHAAN
-        </div>
-        {/* Change lorem ipsum later */}
-        <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec elit id tellus tincidunt tempus eget eget elit. Nullam vitae ornare nibh. Sed tincidunt iaculis odio, tristique pellentesque risus iaculis eget. Integer sit amet tellus eu turpis sollicitudin auctor. Nulla commodo ligula vitae quam vehicula volutpat. Sed urna velit, eleifend.
-        </div>
+      <div className={styles.footerHeader}>
+        <div className={mergeStrings(baseStyles.sectionHeader, styles.whiteText)}>HUBUNGI KAMI</div>
+        <div className={mergeStrings(baseStyles.horizontalDivider, styles.backgroundWhite)}/>
       </div>
-      <div className={styles.column}>
-        <div className={styles.header}>
-          KONTAK
+      <div className={styles.content}>
+        <div className={mergeStrings(styles.column, styles.left)}>
+          <div className={styles.header}>
+            LOKASI
+          </div>
+          <div className={styles.header}>
+            KONTAK
+          </div>
         </div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec elit id tellus tincidunt tempus eget eget elit. Nullam vitae ornare nibh. Sed tincidunt iaculis odio, tristique pellentesque risus iaculis eget. Integer sit amet tellus eu turpis sollicitudin auctor. Nulla commodo ligula vitae quam vehicula volutpat. Sed urna velit, eleifend.
-      </div>
-      <div className={styles.column}>
-        <div className={styles.header}>
-          ALAMAT
+        <div className={mergeStrings(styles.column, styles.right)}>
+          <div className={styles.header}>
+            KONTAK
+          </div>
         </div>
       </div>
     </div>
