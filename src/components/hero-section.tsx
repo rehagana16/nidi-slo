b183963cfbdeffa15ef032fb36bc0542bb31bf28
Rehagana Sembiring <rehagana.sembiring@swiperxapp.com>
2025-02-26 20:09:47 +0700

    add router when hero is clicked

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,9 +1,16 @@
 import Image from 'next/image';
 import styles from './hero-section.module.css';
+import { useRouter } from 'next/router';
 
 export const HeroSection = () => {
+  const router = useRouter();
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      onClick={() => {
+        router.push('/order-form')
+      }}  
+    >
       <Image
         src="/hero-image.png"
         alt="nidi-slo-hero-image"
