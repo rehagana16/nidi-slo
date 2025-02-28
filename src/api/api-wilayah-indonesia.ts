98affc71fb25e16e09d02f6b9ed461ede11c236c
Rehagana Sembiring <rehagana.sembiring@swiperxapp.com>
2025-02-28 17:45:14 +0700

    integrate form with api get wilayah

---
+++ src/api/api-wilayah-indonesia.ts
@@ -0,0 +1,25 @@
+import { AxiosInstance } from "axios";
+import { APIWilayahInstance } from "./axios-instance";
+import { dataKabupatenResponse } from "@/interface/api-wilayah.interface";
+
+class ApiGetWilayahIndonesia {
+  private api: AxiosInstance;
+
+  constructor() {
+    this.api = APIWilayahInstance;
+  }
+
+  async getKabupatenKota() {
+    return this.api.get(`regencies/12.json`);
+  }
+
+  async getKecamatan(regencyId: string) {
+    return this.api.get(`districts/${regencyId}.json`);
+  }
+
+  async getDesa(districtId: string) {
+    return this.api.get(`villages/${districtId}.json`);
+  }
+}
+
+export const apiGetWilayahIndonesia = new ApiGetWilayahIndonesia();
