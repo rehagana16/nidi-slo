955f1f172c46d779e18eb0592a368db5d9a1a953
Rehagana Sembiring <rehagana.sembiring@swiperxapp.com>
2025-03-10 08:06:06 +0700

    feat: head icon desciption meta data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 import './global.css';
 import { Montserrat } from 'next/font/google'
 import Script from 'next/script';
+import Head from 'next/head';
  
 const montserrat = Montserrat({
   weight: ['400', '500', '700'],
@@ -25,6 +26,22 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
  
   return (
     <>
+      <Head>
+        <title>Daftar NIDI SLO SUMUT - NIDI - PLN</title>
+
+        {/* Adding custom favicon */}
+        <link rel="icon" href="/PLN-icon.png" />
+
+        {/* Adding additional information about website */}
+        <meta name="description"
+            content="NIDI SLO PLN SUMUT" />
+        <meta name="keywords"
+            content="NIDI, SLO, NIDI SLO, Daftar NIDI SLO, Daftar NIDI SLO Sumut, Sumut" />
+        <meta name="author"
+            content="Rehagana Sembiring" />
+        <meta name="viewport"
+            content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Script src="https://kit.fontawesome.com/3efa7fb350.js" crossOrigin="anonymous"></Script>
       <main className={montserrat.className}>
         {getLayout(<Component {...pageProps} />)}
